943d3ef6f808879e820de358d587486e1078288e
AnkitLakhani <ankitlakhani52@gmail.com>
2019-03-27 07:36:43 +0000

    Update config.js

--- client/config.js
+++ client/config.js
@@ -3,14 +3,14 @@ module.exports = {
     Invisible reCaptcha site key
     Create one in https://www.google.com/recaptcha/intro/
   */
-  RECAPTCHA_SITE_KEY: '66LeCQJoUAAAAAIXKSSdRXdoW0nqQ6QdanCH3j-1g',
+  RECAPTCHA_SITE_KEY: '6LeCQJoUAAAAAIXKSSdRXdoW0nqQ6QdanCH3j-1g',
 
   // Google analytics tracking ID
   GOOGLE_ANALYTICS_ID: 'UA-136764701-1',
 
   // Contact email address
-  CONTACT_EMAIL: '',
+  CONTACT_EMAIL: 'ankitlakhani52@gmail.com ',
 
   // Report email address
-  REPORT_EMAIL: '',
+  REPORT_EMAIL: 'ankitlakhani52@gmail.com ',
 };
